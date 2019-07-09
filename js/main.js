1a69e52b16952a3b626dcebf990080741ed6da61
Oladipo Osinubi <diposinubi@gmail.com>
2019-07-09 05:40:34 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var CardFour = "king";
+console.log("up and running");
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = []
+var cardOne = cards[0]
+var cardTwo = cards[2]
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo); 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + CardFour);
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again");
+	}
+}
