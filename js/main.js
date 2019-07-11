bf4bbdeb400cfe6848ab71dde587de709b86cab4
Oladipo Osinubi <diposinubi@gmail.com>
2019-07-11 11:40:48 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,43 @@
-console.log("up and running");
-var cards = ["queen", "queen", "king", "king"]
-var cardsInPlay = []
-var cardOne = cards[0]
-var cardTwo = cards[2]
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo); 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2) {
+var cards = [
+{     
+	 rank: "queen", 
+     suit: "hearts",
+     cardImage: "images/queen-of-hearts.png"
+},
+{    
+	 rank: "queen", 
+     suit: "diamonds",
+     cardImage: "images/queen-of-diamonds.png"
+},
+{    
+     rank: "king",
+     suit: "hearts", 
+     cardImage: "images/king-of-hearts.png"
+},
+{   
+     rank:"king",
+     suit: "diamonds",
+     cardImage: "images/king-of-diamonds.png"
+ }
+ ];
+
+var cardsInPlay = [];
+
+ function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
 	} else {
 		console.log("Sorry, try again");
 	}
+};
+
+function flipCard (cardId) {
+	console.log("user flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId.rank]);
+
+checkForMatch();
 }
+  
+  flipCard (0);
+  flipCard (2);
