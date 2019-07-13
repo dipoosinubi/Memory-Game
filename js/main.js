2d13ec044a986a94b1d344ee191fa129f61a4419
Oladipo Osinubi <diposinubi@gmail.com>
2019-07-13 15:38:33 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -32,16 +32,27 @@ var cardsInPlay = [];
 	}
 };
 
-function flipCard (cardId) {
-	console.log("user flipped " + cards[cardId].rank);
+function flipCard () {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	console.log("user flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 
+};
+ function createBoard (){
+ 	for (var i = 0; i < cards.length; i++) {
+ 		var cardElement = document.createElement('img');
+ 		cardElement.setAttribute('src', 'images/back.png');
+ 		cardElement.setAttribute('data-id', i);
+ 		cardElement.addEventListener('click', flipCard);
+ 		document.getElementById('game-board').appendChild(cardElement);
 
-checkForMatch();
-}
-
+ 	}
+ };
   
-  flipCard (0);
-  flipCard (2);
+  createBoard();
